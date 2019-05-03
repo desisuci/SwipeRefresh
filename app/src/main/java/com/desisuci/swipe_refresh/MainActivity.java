ae4f423133844c96bf0b75b027a2a74535ba51f9
Desisuci <desisuci.dsr@gmail.com>
2019-05-03 00:39:39 -0700

    Commit pertama kali

---
+++ app/src/main/java/com/desisuci/swipe_refresh/MainActivity.java
@@ -0,0 +1,23 @@
+package com.desisuci.swipe_refresh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Swipe Refresh Layout
+        mSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swifeRefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new AsyncFetch().execute();
+            }
+        });
+        //Make call to AsyncTask
+        new AsyncFetch().execute();
+    }
+}
